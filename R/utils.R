4098c2ae378d5c278357a04352e88d03757e5e4d
Yihui Xie <xie@yihui.name>
2016-01-07 15:07:47 -0600

    add a sans_serif() function to use sans serif fonts

--- R/utils.R
+++ R/utils.R
@@ -50,6 +50,20 @@ quote_footer = function(text) {
   }
 }
 
+#' @details \code{sans_serif()} applies sans-serif fonts to \code{text}.
+#' @rdname tufte_handout
+#' @export
+sans_serif = function(text) {
+  if (is_html_output()) {
+    sprintf('<span class="sans">%s</span>', text)
+  } else if (is_latex_output()) {
+    sprintf('\\textsf{%s}', text)
+  } else {
+    warning('sans_serif() only works for HTML and LaTeX output', call. = FALSE)
+    text
+  }
+}
+
 template_resources = function(name, ...) {
   system.file('rmarkdown', 'templates', name, 'resources', ..., package = 'tufte')
 }
