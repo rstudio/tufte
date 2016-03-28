99291dba5564cda8516592321a960c81f292dc66
Yihui Xie <xie@yihui.name>
2016-03-28 15:43:05 -0500

    provide an option to escape text for functions newthought(), margin_note(), quote_footer(), and sans_serif()

--- R/utils.R
+++ R/utils.R
@@ -4,18 +4,14 @@
 #'   (\samp{\\newthought{text}}) output.
 #' @param text A character string to be presented as a \dQuote{new thought}
 #'   (using small caps), or a margin note, or a footer of a quote
+#' @param escape Whether to escape special HTML/LaTeX characters in \code{text}
 #' @rdname tufte_handout
 #' @export
 #' @examples newthought('In this section')
-newthought = function(text) {
-  if (is_html_output()) {
-    sprintf('<span class="newthought">%s</span>', text)
-  } else if (is_latex_output()) {
-    sprintf('\\newthought{%s}', text)
-  } else {
-    sprintf('<span style="font-variant:small-caps;">%s</span>', text)
-  }
-}
+newthought = function(text, escape = TRUE) cond_text(
+  text, escape, '<span class="newthought">%s</span>', '\\newthought{%s}',
+  sprintf('<span style="font-variant:small-caps;">%s</span>', text)
+)
 
 #' @details \code{margin_note()} can be used in inline R expressions to write a
 #'   margin note (like a sidenote but not numbered).
@@ -23,45 +19,45 @@ newthought = function(text) {
 #'   the page width is too narrow (by default it is a circled plus sign)
 #' @rdname tufte_handout
 #' @export
-margin_note = function(text, icon = '&#8853;') {
-  if (is_html_output()) {
-    marginnote_html(sprintf('<span class="marginnote">%s</span>', text), icon)
-  } else if (is_latex_output()) {
-    sprintf('\\marginnote{%s}', text)
-  } else {
+margin_note = function(text, icon = '&#8853;', escape = TRUE) cond_text(
+  text, escape,
+  sprintf('%s<span class="marginnote">%%s</span>', marginnote_html('', icon)),
+  '\\marginnote{%s}', {
     warning('marginnote() only works for HTML and LaTeX output', call. = FALSE)
     text
   }
-}
+)
 
 #' @details \code{quote_footer()} formats text as the footer of a quote. It puts
 #'   \code{text} in \samp{<footer></footer>} for HTML output, and
 #'   after \samp{\\hfill} for LaTeX output (to right-align text).
 #' @rdname tufte_handout
 #' @export
-quote_footer = function(text) {
-  if (is_html_output()) {
-    sprintf('<footer>%s</footer>', text)
-  } else if (is_latex_output()) {
-    sprintf('\\hfill %s', text)
-  } else {
+quote_footer = function(text, escape = TRUE) cond_text(
+  text, escape, '<footer>%s</footer>', '\\hfill %s', {
     warning('quote_footer() only works for HTML and LaTeX output', call. = FALSE)
     text
   }
-}
+)
 
 #' @details \code{sans_serif()} applies sans-serif fonts to \code{text}.
 #' @rdname tufte_handout
 #' @export
-sans_serif = function(text) {
-  if (is_html_output()) {
-    sprintf('<span class="sans">%s</span>', text)
-  } else if (is_latex_output()) {
-    sprintf('\\textsf{%s}', text)
-  } else {
+sans_serif = function(text, escape = TRUE) cond_text(
+  text, escape, '<span class="sans">%s</span>', '\\textsf{%s}', {
     warning('sans_serif() only works for HTML and LaTeX output', call. = FALSE)
     text
   }
+)
+
+cond_text = function(text, escape = TRUE, html, latex, other) {
+  if (is_html_output()) {
+    if (escape) text = knitr:::escape_html(text)
+    sprintf(html, text)
+  } else if (is_latex_output()) {
+    if (escape) text = knitr:::escape_latex(text)
+    sprintf(latex, text)
+  } else other
 }
 
 template_resources = function(name, ...) {
