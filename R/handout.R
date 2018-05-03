261004a1b97eba251c8cc85d6145b739e999d64a
Kevin N. Murphy <knmurphy@users.noreply.github.com>
2018-05-03 14:56:36 -0400

    changing to section in hopes of removing blank page between sections

--- R/handout.R
+++ R/handout.R
@@ -49,7 +49,7 @@ tufte_pdf = function(
   format$pandoc$args = c(
     format$pandoc$args, '--variable', paste0('documentclass:', documentclass),
     if (documentclass == 'tufte-book')
-      '--top-level-division=chapter'
+      '--top-level-division=section'
   )
 
   knitr::knit_engines$set(marginfigure = function(options) {
