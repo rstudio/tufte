137c3d57482fdecc727017323b452fece9909825
christophe dervieux <christophe.dervieux@gmail.com>
2021-01-25 22:48:31 +0100

    refactor test by using a knitr template for the Rmd

--- tests/testthat/test-html.R
+++ tests/testthat/test-html.R
@@ -4,40 +4,31 @@ test_that("add marginnote", {
 })
 
 
+expect_refs_margin <- function(moved = FALSE) {
+  rmd <- test_path("resources/margins_references.Rmd")
+  out <- withr::local_tempfile(fileext = ".html")
+  rmd_temp <- withr::local_tempfile(fileext = ".Rmd")
+  xfun::write_utf8(
+    knitr::knit_expand(rmd, linked = if (moved) "yes" else "no"),
+    rmd_temp
+  )
+  rmarkdown::pandoc_convert(basename(rmd_temp), "html4", "markdown",
+                                   output = out, citeproc = TRUE, verbose = FALSE,
+                                   wd = dirname(rmd_temp))
+  x <- xfun::read_utf8(out)
+  expect_snapshot(margin_references(x))
+}
+
 test_that("put references in margin when link-citations: yes using Pandoc 2.11+", {
   skip_on_cran() # requires recent Pandoc
   skip_if_not(rmarkdown::pandoc_available("2.11"))
-  rmd <- test_path("resources/margins_references.Rmd")
-  out <- withr::local_tempfile(fileext = ".html")
-  res <- rmarkdown::pandoc_convert(basename(rmd), "html4", "markdown",
-                                   output = out, citeproc = TRUE, verbose = FALSE,
-                                   wd = dirname(rmd))
-  x <- xfun::read_utf8(out)
-  expect_snapshot(margin_references(x))
-  xfun::gsub_file(rmd, pattern = "^link-citations: yes$", replacement = "link-citations: no")
-  res <- rmarkdown::pandoc_convert(basename(rmd), "html4", "markdown",
-                                   output = out, citeproc = TRUE, verbose = FALSE,
-                                   wd = dirname(rmd))
-  x <- xfun::read_utf8(out)
-  expect_snapshot(margin_references(x))
-  xfun::gsub_file(rmd, pattern = "^link-citations: no$", replacement = "link-citations: yes")
+  expect_refs_margin(moved = TRUE)
+  expect_refs_margin(moved = FALSE)
 })
 
 test_that("put references in margin when link-citations: yes before Pandoc 2.11+", {
   skip_on_cran() # requires recent Pandoc
   skip_if(rmarkdown::pandoc_available("2.11"))
-  rmd <- test_path("resources/margins_references.Rmd")
-  out <- withr::local_tempfile(fileext = ".html")
-  res <- rmarkdown::pandoc_convert(basename(rmd), "html4", "markdown",
-                                   output = out, citeproc = TRUE, verbose = FALSE,
-                                   wd = dirname(rmd))
-  x <- xfun::read_utf8(out)
-  expect_snapshot(margin_references(x))
-  xfun::gsub_file(rmd, pattern = "^link-citations: yes$", replacement = "link-citations: no")
-  res <- rmarkdown::pandoc_convert(basename(rmd), "html4", "markdown",
-                                   output = out, citeproc = TRUE, verbose = FALSE,
-                                   wd = dirname(rmd))
-  x <- xfun::read_utf8(out)
-  expect_snapshot(margin_references(x))
-  xfun::gsub_file(rmd, pattern = "^link-citations: no$", replacement = "link-citations: yes")
+  expect_refs_margin(moved = TRUE)
+  expect_refs_margin(moved = FALSE)
 })
